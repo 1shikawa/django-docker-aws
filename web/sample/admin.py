c09dea8267cc2721a133cc2280726b392feca08b
1shikawa <ishikawa.toru@gmail.com>
2019-04-25 01:20:24 +0900

    custome command add

--- web/sample/admin.py
+++ web/sample/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Sample
+
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+admin.site.register(Sample, SampleAdmin)
