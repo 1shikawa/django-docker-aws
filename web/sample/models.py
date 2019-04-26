aa383f4b74cee59a6842bb22731be11605d59e3e
1shikawa <ishikawa.toru@gmail.com>
2019-04-26 18:40:17 +0900

    custome command add

--- web/sample/models.py
+++ web/sample/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Sample(models.Model):
+# class Sample(models.Model):
+#     title = models.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return self.title
+
+class Test(models.Model):
     title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
+
