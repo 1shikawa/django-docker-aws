f6ca784714acdc819cb6eb4268fd68c037773bf6
1shikawa <ishikawa.toru@gmail.com>
2019-04-26 18:55:24 +0900

    custome command add

--- web/sample/admin.py
+++ web/sample/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Sample
+from .models import Sample, Test
 
 class SampleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
 
 admin.site.register(Sample, SampleAdmin)
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+admin.site.register(Sample, SampleAdmin)
+admin.site.register(Test, TestAdmin)
--- web/sample/models.py
+++ web/sample/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-# class Sample(models.Model):
-#     title = models.CharField(max_length=255)
-#
-#     def __str__(self):
-#         return self.title
+class Sample(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
 class Test(models.Model):
     title = models.CharField(max_length=255)
